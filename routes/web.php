8f45700835ea1e9994a303a953e4ef4f36c2bb0e
Raja Abu Salameh <future.adv2@gmail.com>
2017-08-26 01:10:09 +0300

    add one signal support

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,37 @@ Route::get('/', function () {
     return redirect('/app');
 });
 
+Route::get('/one-signal', function(){
+	$content = array(
+			"en" => 'English Message'
+			);
+		
+		$fields = array(
+			'app_id' => "9d4fcc12-cb93-4aa7-a8e3-38a8ca2fca06",
+			'included_segments' => array('All'),
+      		'data' => array("foo" => "bar"),
+			'contents' => $content
+		);
+		
+		$fields = json_encode($fields);
+    	print("\nJSON sent:\n");
+    	print($fields);
+		
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_URL, "https://onesignal.com/api/v1/notifications");
+		curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json; charset=utf-8',
+												   'Authorization: Basic YzdhMDlhYmItNjY2NC00Y2U5LTg0OWYtMDE2ZGRjZDczMGMy'));
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+		curl_setopt($ch, CURLOPT_HEADER, FALSE);
+		curl_setopt($ch, CURLOPT_POST, TRUE);
+		curl_setopt($ch, CURLOPT_POSTFIELDS, $fields);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
 
+		$response = curl_exec($ch);
+		curl_close($ch);
+		
+		return $response;
+});
 Auth::routes();
 
 Route::prefix('app')->middleware(['auth'])->group(function () {
